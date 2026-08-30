5f9c3570d1a716dadfb747c7cbf2f1f760f5e110
Olga Gorban <oatyanina17@gmail.com>
2026-08-30 13:42:46 +0300

    Address review comments: verify error message, use build.gradle for chromeoptions, remove manual ChromeOptions and redundant driver close

--- src/test/java/ru/netology/page/TransferPage.java
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -6,6 +6,7 @@ import ru.netology.data.DataHelper;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -35,5 +36,6 @@ public class TransferPage {
 
     public void findErrorNotification() {
         errorNotification.shouldBe(visible, Duration.ofSeconds(15));
+        errorNotification.shouldHave(text("Ошибка"));
     }
 }
--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -1,41 +1,17 @@
 package ru.netology.test;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPage;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoneyTransferTest {
 
-    @BeforeAll
-    static void setupChrome() {
-        ChromeOptions options = new ChromeOptions();
-
-        Map<String, Object> prefs = new HashMap<>();
-        prefs.put("credentials_enable_service", false);
-        prefs.put("profile.password_manager_enabled", false);
-        prefs.put("profile.password_manager_leak_detection", false);
-
-        options.setExperimentalOption("prefs", prefs);
-        options.addArguments("--disable-features=PasswordLeakDetection,PasswordCheck,AutofillEnableAccountWalletStorage");
-        options.addArguments("--disable-save-password-bubble");
-
-        Configuration.browserCapabilities = options;
-    }
-
     @BeforeEach
     void setup() {
-        Selenide.closeWebDriver();
         open("http://localhost:9999");
     }
 
